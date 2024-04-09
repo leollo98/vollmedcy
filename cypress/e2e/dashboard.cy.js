4a399df4b098c37236102a38f7e2f726be297bba
camilapessoa <camilabentespessoa@gmail.com>
2024-04-09 15:56:41 -0300

    video 2.4 automatizando captura checkbox

--- cypress/e2e/dashboard.cy.js
+++ cypress/e2e/dashboard.cy.js
@@ -2,16 +2,39 @@ describe('Usuário logado na página de dashboard', () => {
     beforeEach(() => {
         cy.login(Cypress.env('email'), Cypress.env('senha'))
     })
-    it('Verifica página de redirecionamento no login com sucesso', () => {
-        cy.visit('/dashboard')
-        cy.url().should('eq', 'http://localhost:3000/dashboard')
+
+    context('Redirecionamento na página de dashboard', () => {
+        it('Verifica página de redirecionamento no login com sucesso', () => {
+            cy.visit('/dashboard')
+            cy.url().should('eq', 'http://localhost:3000/dashboard')
+        })
+
+        it('Com o usuário logado, cadastra um especialista', () => {
+            cy.visit('/dashboard')
+            cy.url().should('eq', 'http://localhost:3000/dashboard')
+            cy.contains('Cadastrar especialista').should('be.visible').click()
+
+
+        })
+
     })
 
-    it('Com o usuário logado, cadastra um especialista',() => {
-        cy.visit('/dashboard')
-        cy.url().should('eq', 'http://localhost:3000/dashboard')
-        cy.contains('Cadastrar especialista').should('be.visible').click()
+    context('Modal de cadastro de especialista', () => {
+        it('Verifica se o checkbox "Atende por plano?" está desmarcado', () => {
+            cy.visit('/dashboard')
+            cy.contains('Cadastrar especialista').should('be.visible').click()
+            cy.get('[type="checkbox"]').should('have.attr', 'aria-label', 'Atende por plano?').and('not.be.checked')
+        })
 
+        it.only('Seleciona o botão checkbox "Atende por plano?" para visualizar os planos de saúde', () => {
+            cy.visit('/dashboard')
+            cy.contains('Cadastrar especialista').should('be.visible').click()
+            cy.get('[type="checkbox"]').check()
+            cy.get('form').find('input[type="checkbox"]').should('be.checked').and('not.be.disabled')
+            cy.get('[type="checkbox"]').check(['Sulamerica', 'Unimed', 'Bradesco'])
+        })
+
+
+        })
 
     })
-})
