8128c4ec7fa60ba8c78c2028a4fcfbf87e9424e4
camilapessoa <camilabentespessoa@gmail.com>
2024-04-09 09:11:43 -0300

    configuracao viewport

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,11 @@
+Cypress.Commands.add('login', (email, senha) => {
+  
+    cy.visit('/login') 
+    cy.get('[data-test="inputLoginEmail"]').type(email)
+    cy.get('[data-test="inputLoginSenha"]').type(senha, {log:false})
+    cy.get('[data-test="botaoTeste"]').should('be.visible').click()
+})
+
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
